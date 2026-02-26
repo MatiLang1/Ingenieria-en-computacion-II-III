eed7a00eeec2ab08b7910d5568c2fe74f2fca408
Matias Lang <matiaslang124@gmail.com>
2026-02-26 17:51:12 -0300

    Comentarios en ESP32

--- Entorno_real/ESP32.c
+++ Entorno_real/ESP32.c
@@ -19,53 +19,60 @@ const char* password = WIFI_PASSWORD;
 const char* mqtt_server = MQTT_SERVER_IP; 
 const int mqtt_port = 1883; 
 
-// Configuración PWM para Buzzer Pasivo (Compatible ESP32 Core v3.0)
+// Configuración PWM para Buzzer Pasivo
 const int pwmFreq = 2000;
 const int pwmResolution = 8;
-const int pwmDuty = 128; // 50% duty cycle
+const int pwmDuty = 128; // 50% duty cycle para q parpadee
 
-// Buzzer vars
+// Variables para Buzzer
 unsigned long tiempoInicioBuzzer = 0;
 bool buzzerState = false;
-const unsigned long DURACION_BUZZER = 15000; 
+const unsigned long DURACION_BUZZER = 15000; // 15 segundos
 
-// LED vars
+// Variables para LED
 unsigned long ultimo_parpadeo = 0;
 bool ledState = LOW;
-const unsigned long intervalo_parapadeo = 500; 
+const unsigned long intervalo_parapadeo = 500; // 500 milisegundos
 
-WiFiClient espClient;
-PubSubClient client(espClient);
-DHT dht(DHTPIN, DHTTYPE);
+WiFiClient espClient; // creamos el objeto espClient (instancia de WiFiClient)
+PubSubClient client(espClient); // creamos el objeto client (instancia de PubSubClient) y le pasamos el objeto espClient para q este ultimo se encargue de la conexion WiFi (client se encarga de la comunicacion MQTT y espClient de la conexion WiFi, aca usamos inyeccion de dependencias)
+DHT dht(DHTPIN, DHTTYPE); // creamos el objeto dht (instancia de DHT) y le pasamos los parametros (el pin y el tipo de sensor)
 
-String estadoSistema = "NORMAL";
+String estadoSistema = "OK"; // variable para almacenar el estado del sistema
 
 unsigned long ultima_publicacion = 0;
 const unsigned long intervalo_publicacion = 5000; 
 
+//Configuracion de WiFi (el objeto Wifi es una instancia q ya trae la libreria)
 void setupWiFi() {
   WiFi.begin(ssid, password);
   while (WiFi.status() != WL_CONNECTED) delay(500);
 }
 
+
+// client llama a esta funcion cuandoel broker le avisa a la esp32 q llego un nuevo msj. La libreria PubSubClient manda los parametros topic, payload y length (envia la longitud del msj pq al mandar bytes en el payload no se sabe donde termina)
+// FUNCION QUE RECIBE EL MSJ QUE PUBLICA LA PAGINA WEB
 void callback(char* topic, byte* payload, unsigned int length) {
   String msg;
-  for (int i = 0; i < length; i++) msg += (char)payload[i];
+  for (int i = 0; i < length; i++) msg += (char)payload[i]; // convertimos los bytes en string (en caracteres y los concatenamos para formar el mensaje)
 
+  // String(topic) pasa topic de char* a String asi validamos q sea el topico correcto, dsp hacemos una AND y en la segunda comdicion validamos q la palabra OFF este en el msj
   if (String(topic) == "semillero/control/buzzer" && msg.indexOf("OFF") != -1) {
-    ledcWrite(BUZZER, 0); // Apagar PWM (Core v3 usa Pin)
-    buzzerState = false; 
+    ledcWrite(BUZZER, 0); // apagamos el buzzer
+    buzzerState = false; // cambiamos flag de buzzer
     client.publish("semillero/estado/buzzer", "{ \"buzzer\": \"OFF (Manual)\" }");
   }
 }
 
+// FUNCION QUE CONECTA LA ESP32 AL BROKER MQTT (reintenta conex cada 2 segundos)
 void reconnectMQTT() {
   while (!client.connected()) {
     Serial.print("Intentando conectar a MQTT IP: ");
     Serial.println(mqtt_server);
-    if (client.connect("ESP32_Semillero")) {
+    //el metodo connect acepta 3 parametros (ID, user y password), en el broker habilitamos conex anonimas asique con pasarle el ID es suficiente (en este caso el ID es "ESP32_Semillero" el cual usa el broker para reconocer a nuestra ESP32)
+    if (client.connect("ESP32_Semillero")) { // si se conecta al broker
       Serial.println("Conectado a broker MQTT");
-      client.subscribe("semillero/control/buzzer");
+      client.subscribe("semillero/control/buzzer"); // nos suscribimos al topic donde la pag web publica
     } else {
       Serial.print("Falló MQTT, rc=");
       Serial.print(client.state());
@@ -86,26 +93,31 @@ void evaluarEstado(float temp, int luz, int nivel) {
   bool alerta = (temp > TEMP_ALERTA) || (luz > LUZ_ALERTA) || (nivel < NIVEL_ALERTA);
   bool aviso = (temp > TEMP_AVISO) || (luz > LUZ_AVISO) || (nivel < NIVEL_AVISO);
 
+  // SI ENTRA EN ESTADO DE ALERTA
   if (alerta) {
     estadoSistema = "ALERTA";
     
-    // Forzamos PWM ON SIEMPRE que haya alerta (por si acaso se apago)
+    // Prendemos buzzer (mandamos una señal pwm, le pasamos de argumento el pin y el duty cycle)
     ledcWrite(BUZZER, pwmDuty); 
 
-    // Solo logueamos/publicamos si el estado cambió a TRUE
+    // Solo publicamos si el estado cambió a TRUE
     if (!buzzerState) {
         buzzerState = true;
-        Serial.println("ALERTA detectada! Buzzer ON (PWM)");
-        // Forzamos el envio inmediato
+        Serial.println("ALERTA detectada! Buzzer ON");
+        // publicamos el estado del buzzer para q se pueda visualizar de forma inmediata en la seccion "Tiempo Real" de la pagina web
         client.publish("semillero/estado/buzzer", "{ \"buzzer\": \"ON\" }");
     }
     
     tiempoInicioBuzzer = millis(); // Reinicia temporizador
   } 
+
+  // SI ENTRA EN ESTADO DE AVISO
   else if (aviso) {
-     estadoSistema = "AVISO";
+    estadoSistema = "AVISO";
     Serial.println("Estado del sistema: AVISO");
   }
+
+  // SI ENTRA EN ESTADO OK (al no entrar en Alerta ni Aviso)
   else {
     estadoSistema = "OK";
     Serial.println("Estado del sistema: OK");
@@ -113,8 +125,10 @@ void evaluarEstado(float temp, int luz, int nivel) {
 }
 
 void publishAll(float temp, int luz, int nivel) {
-  char buf[80];
+  char buf[80]; // en C para concatenar debemos crear un buffer. Lo hacemos para almacenar los mensajes (reservamos memoria para 80 caracteres)
 
+  // sprintf formatea el mensaje pero no lo imprime, lo guarda en el buffer "buf" y luego publicamos el buffer el cual contiene el mensaje/payload. En los siguiente sprintf se hace lo mismo sobreescribiendo "buf"
+  // El true es para q el broker retenga el ultimo valor publicado
   sprintf(buf, "{ \"value\": %.2f }", temp);
   if (client.publish("semillero/sensores/temperatura", buf, true)) {
     Serial.println("Publicado temperatura: OK");
@@ -122,6 +136,7 @@ void publishAll(float temp, int luz, int nivel) {
     Serial.println("Publicado temperatura: FALLO");
   }
 
+  //en cada publicacion se hace lo mismo, formateamos el msj usando el buffer "buf" (q se sobreescribe por cada topic) y publicamos
   sprintf(buf, "{ \"value\": %d }", luz);
   if (client.publish("semillero/sensores/luz", buf, true)) {
     Serial.println("Publicado luz: OK");
@@ -139,7 +154,6 @@ void publishAll(float temp, int luz, int nivel) {
   sprintf(buf, "{ \"estado\": \"%s\" }", estadoSistema.c_str());
   client.publish("semillero/estado/sistema", buf, true);
 
-  // Tambien publicamos aqui para asegurar redundancia
   client.publish("semillero/estado/buzzer",
     buzzerState ? "{ \"buzzer\": \"ON\" }" : "{ \"buzzer\": \"OFF\" }", true);
 }
@@ -149,47 +163,41 @@ void setup() {
   delay(1000);
   Serial.println("ESP32 iniciando...");
 
+  // Configuración de pines de LEDs
   pinMode(LED_VERDE, OUTPUT);
   pinMode(LED_ROJO, OUTPUT);
   
-  // Agregar pinMode por seguridad
+  // Agregamos pinMode por seguridad
   pinMode(BUZZER, OUTPUT);
 
-  // Configuración PWM (Core v3.0+)
-  // ledcAttach(pin, freq, resolution);
+  // Configuramos PWM en el pin del buzzer. ledcAttach(pin, freq, resolution)
   if (!ledcAttach(BUZZER, pwmFreq, pwmResolution)) {
       Serial.println("Error al adjuntar LEDC!");
   }
 
-  dht.begin();
+  dht.begin(); // inicializamos el sensor de temperatura
 
-  Serial.println("Conectando a WiFi...");
+  Serial.println("Conectando a WiFi");
   setupWiFi();
   Serial.println("WiFi conectado");
   Serial.print("IP ESP32: ");
   Serial.println(WiFi.localIP());
 
-  client.setServer(mqtt_server, mqtt_port);
-  client.setCallback(callback);
+  client.setServer(mqtt_server, mqtt_port); // establecemos la direccion del broker (IP + Puerto)
+  client.setCallback(callback); // establecemos la funcion que se ejecutara cuando se reciba un mensaje MQTT
 
-  // Test Sonido Inicio
-  ledcWrite(BUZZER, pwmDuty);
-  delay(200);
-  ledcWrite(BUZZER, 0);
-
-  Serial.println("Setup finalizado");
 }
 
 void loop() {
-  if (!client.connected()) reconnectMQTT();
-  client.loop();
+  if (!client.connected()) reconnectMQTT(); // si no esta conectado al broker, intenta reconectarse
+  client.loop(); // mantiene viva la conexion con el broker
 
-  // Logica de apagado del buzzer (timer)
+  // Logica de apagado del buzzer (timer de 15s)
   if (buzzerState) {
-    // Si ya pasaron 15s desde la ÚLTIMA recarga
     if (millis() - tiempoInicioBuzzer >= DURACION_BUZZER) {
-      ledcWrite(BUZZER, 0); // Apagar PWM (Core v3 usa Pin)
+      ledcWrite(BUZZER, 0); // Apagamos PWM
       buzzerState = false;
+      // publicamos el estado del buzzer de forma instantanea para q se pueda visualizar en la seccion "Tiempo Real" de la pagina web
       client.publish("semillero/estado/buzzer", "{ \"buzzer\": \"OFF\" }");
       Serial.println("Buzzer apagado por Tiempo");
     }
@@ -197,7 +205,6 @@ void loop() {
 
   // Logica de los LEDs
   digitalWrite(LED_VERDE, HIGH); 
-
   if (estadoSistema == "ALERTA") {
     digitalWrite(LED_ROJO, HIGH); 
   } 
@@ -212,13 +219,15 @@ void loop() {
     digitalWrite(LED_ROJO, LOW); 
   }
 
+  // Leemos sensores y publicamos valores periodicamente (cada 5 segundos)
   if (millis() - ultima_publicacion >= intervalo_publicacion) {
     ultima_publicacion = millis();
+
+    // Guardamos en variables las lecturas de los sensores y las pasamos a las funciones evaluarEstado y publishAll
     float temp = dht.readTemperature();
     int luz = analogRead(LDR_PIN);
     int nivel = analogRead(NIVEL_PIN);
-
     evaluarEstado(temp, luz, nivel); 
-    publishAll(temp, luz, nivel);
+    publishAll(temp, luz, nivel); 
   }
 }
